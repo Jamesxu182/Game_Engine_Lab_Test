bb341c26fb6e4637af3747dc5a8e18d9060b0159
Yifan Xu <Yifan Xu>
2017-05-05 16:04:52 +0100

    Completed

---
+++ Assets/Atom.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Atom : MonoBehaviour {
+    public string electronsPerShell;
+    public string atom;
+    public GameObject protonPrefab;
+    public GameObject electronPrefab;
+
+    public int shellSmoothRate = 20;
+
+    private List<List<Vector3>> shells = new List<List<Vector3>>();
+
+    private List<int> electronsNumbers = new List<int>();
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        for(int i = 0; i < shells.Count; i++)
+        {
+            for (int j = 0; j < shellSmoothRate; j++)
+            {
+                Vector3 current = shells[i][j];
+                Vector3 next = j == (shellSmoothRate - 1) ? shells[i][0] : shells[i][j + 1];
+
+                Gizmos.DrawSphere(transform.TransformPoint(current), 0.1f);
+                Gizmos.DrawLine(transform.TransformPoint(current), transform.TransformPoint(next));
+            }
+        }
+
+    }
+
+    // Use this for initialization
+    void Start () {
+        ParseElectronString();
+
+        CreateShells();
+
+        CreateProton();
+        CreateElectrons();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    private void CreateShells()
+    {
+        float interval = 2 * Mathf.PI / shellSmoothRate;
+
+        for(int i = 0; i < electronsNumbers.Count; i++)
+        {
+            List<Vector3> shell = new List<Vector3>();
+
+            for(int j = 0; j < shellSmoothRate; j++)
+            {
+                shell.Add(new Vector3(Mathf.Sin(j * interval) * (i + 1) * 5.0f, 0, Mathf.Cos(j * interval) * (i + 1) * 5.0f));
+            }
+
+            shells.Add(shell);
+        }
+    }
+
+    private void CreateProton()
+    {
+        if(protonPrefab)
+        {
+            GameObject proton = GameObject.Instantiate(protonPrefab);
+            proton.transform.parent = transform;
+
+            //proton.transform.position = transform.InverseTransformPoint(transform.position);
+            proton.transform.position = transform.position;
+        }
+    }
+
+    private void CreateElectrons()
+    {
+        if(electronPrefab)
+        {
+
+            for(int i = 0; i < shells.Count; i++)
+            {
+                float interval = 2 * Mathf.PI / electronsNumbers[i];
+                for(int j = 0; j < electronsNumbers[i]; j++)
+                {
+                    GameObject electron = GameObject.Instantiate(electronPrefab);
+                    electron.transform.parent = transform;
+                    electron.transform.position = transform.position + new Vector3(Mathf.Sin(j * interval) * (i + 1) * 5.0f, 0, Mathf.Cos(j * interval) * (i + 1) * 5.0f);
+                    electron.GetComponent<FollowPathBehaviour>().shell = shells[i];
+                    electron.GetComponent<FollowPathBehaviour>().centre = transform.position;
+                }
+            }
+        }
+    }
+
+    private void ParseElectronString()
+    {
+        char[] seperators = {','};
+        string[] numbers = electronsPerShell.Split(seperators);
+
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            electronsNumbers.Add(int.Parse(numbers[i]));
+        }
+    }
+}
---
+++ Assets/FollowPathBehaviour.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPathBehaviour : SteeringBehaviour
+{
+    public List<Vector3> shell;
+
+    private int index = 0;
+
+    public Vector3 centre = Vector3.zero;
+
+    // Use this for initialization
+    void Start()
+    {
+        FindTheCloestPoint();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public override Vector3 Calculate()
+    {
+        Vector3 next = shell[index];
+
+        if (Vector3.Distance(transform.position, next + centre) < 1.0f)
+        {
+            if(index == (shell.Count -1))
+            {
+                index = 0;
+            }
+
+            else
+            {
+                index++;
+            }
+
+        }
+
+        return boid.SeekForce(next + centre);
+    }
+
+    private void FindTheCloestPoint()
+    {
+        float minDistance = 0;
+        if(shell.Count > 0)
+        {
+            minDistance = Vector3.Distance(shell[0] + centre, transform.position);
+            index = 0;
+        }
+
+        for(int i = 1; i < shell.Count; i++)
+        {
+            float distance = Vector3.Distance(shell[i] + centre, transform.position);
+            if(distance < minDistance)
+            {
+                minDistance = distance;
+                index = i;
+            }
+        }
+    }
+}
